e417a4dd02305ab6066a8d54e859c6370952992f
LASGLOWTECH <omozemojeau@gmail.com>
2025-08-12 20:23:52 +0100

    made changes

--- src/components/loader.jsx
+++ src/components/loader.jsx
@@ -25,7 +25,7 @@ const SiteLoader = () => {
 
   return (
    <div
-  className={`fixed inset-0 bg-gradient-to-r from-bgcolor2 to-bgcolor2 z-50 transition-opacity duration-500 ${
+  className={`fixed inset-0 bg-bgcolor z-50 transition-opacity duration-500 ${
     fadeOut ? "opacity-0" : "opacity-100"
   }`}
 >
